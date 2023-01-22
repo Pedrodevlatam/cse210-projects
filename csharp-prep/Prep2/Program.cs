2d9788bfda4c881bdaa2259ac428cb74aa6e0535
Pedrodevlatam <pamp1891@gmail.com>
2023-01-22 00:34:48 -0500

    Prep2 update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,41 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+class Program{
+  public static void Main (string[] args) {
+    Console.WriteLine("Enter your grade percentage:");
+    int grade = int.Parse(Console.ReadLine());
+    string letter = "";
+    string sign = "";
+
+    if (grade >= 90) {
+      letter = "A";
+    } else if (grade >= 80) {
+      letter = "B";
+    } else if (grade >= 70) {
+      letter = "C";
+    } else if (grade >= 60) {
+      letter = "D";
+    } else {
+      letter = "F";
     }
+
+    if (letter != "A" && letter != "F") {
+      int lastDigit = grade % 10;
+      if (lastDigit >= 7) {
+        sign = "+";
+      } else if (lastDigit < 3) {
+        sign = "-";
+      }
+    }
+
+    Console.WriteLine("Your letter grade is " + letter + sign);
+
+    if (letter != "F") {
+      Console.WriteLine("Congratulations, you passed the class!");
+    } else {
+      Console.WriteLine("Unfortunately, you did not pass the class. Keep working hard for next time.");
+    }
+  }
 }
+
+
